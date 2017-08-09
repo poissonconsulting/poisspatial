da31455bb725a976680a633fef49b3584eb04055
Joe Thorley <joe@poissonconsulting.ca>
2017-08-09 15:46:23 -0700

    recursive

--- R/blob.R
+++ R/blob.R
@@ -3,23 +3,25 @@
 #' @param dir A string of the directory
 #' @param ext A string of the file extension(s)
 #' @param n An integer of the (maximal) number of records to be read.
+#' @param recursive A flag indicating whether no recurse into subdirectories.
 #' @seealso readBin
 #' @return A tibble with the columns FileName and BLOB
 #' @export
-blob <- function(dir, ext = "[.]pdf$", n =  10000L) {
+blob <- function(dir, ext = "[.]pdf$", n =  10000L, recursive = TRUE) {
   check_string(dir)
   check_string(ext)
   check_count(n)
+  check_flag(recursive)
 
   if (!dir.exists(dir))
     stop("directory '", dir, "' does not exist", call. = FALSE)
 
-  files <- list.files(dir, pattern = ext, full.names = TRUE, recursive = TRUE)
+  files <- list.files(dir, pattern = ext, full.names = TRUE, recursive = recursive)
 
   if (!length(files))
     return(tibble::tibble(FileName = character(0), BLOB = integer(0)))
 
-  sub <- list.files(dir, pattern = ext, recursive = TRUE)
+  sub <- list.files(dir, pattern = ext, recursive = recursive)
   sub <- dirname(sub)
 
   filenames <- basename(files)
