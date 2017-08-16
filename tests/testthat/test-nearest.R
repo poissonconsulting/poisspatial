dc1ad363b71c3d7f59ed4ecc62813d828eddbaf1
Joe Thorley <joe@poissonconsulting.ca>
2017-08-16 10:54:06 -0700

    more nearest

--- tests/testthat/test-nearest.R
+++ tests/testthat/test-nearest.R
@@ -28,3 +28,17 @@ test_that("nearest", {
   expect_identical(n$Distance, as.difftime(c(-4, -1, 3), units = "days"))
 })
 
+test_that("nearest2", {
+
+  x <- data.frame(Date = ISOdate(2000, 1, c(9, 1, 4)))
+  y <- data.frame(Date = ISOdate(2000, 1, c(12, 5, 6)))
+  y$Row <- 1:nrow(x)
+
+  n <- ps_nearest(x, y, "Date")
+  expect_is(n, "tbl_df")
+  expect_identical(colnames(n), c("Date", "Row", "Distance"))
+  expect_identical(n$Date, x$Date)
+  expect_identical(n$Row, c(3L, 2L, 2L))
+  expect_identical(n$Distance, as.difftime(c(3, -4, -1), units = "days"))
+})
+
