7a0767db516db0230c9448d69c75ab41594a3a7b
Joe Thorley <joe@poissonconsulting.ca>
2017-08-17 11:12:49 -0700

    moved ps_blob to poissqlite

--- tests/testthat/test-blob.R
+++
@@ -1,9 +0,0 @@
-context("blob")
-
-test_that("blob works", {
-  dir <- file.path(system.file(package = "poisspatial"))
-  blob <- ps_blob(dir)
-  expect_identical(colnames(blob), c("FileName", "SubDirectory", "BLOB"))
-  expect_identical(blob$FileName, c("seb-dalgarno.pdf", "joe-thorley.pdf"))
-  expect_identical(blob$SubDirectory, c(".", "sub"))
-})
