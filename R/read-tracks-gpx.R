c1ced81d48d2794228cfc2defe9baf7c8e57c34d
Joe Thorley <joe@poissonconsulting.ca>
2017-08-24 17:12:28 -0700

    read tracks gpxs

--- R/read-tracks-gpx.R
+++ R/read-tracks-gpx.R
@@ -6,6 +6,7 @@
 #' @param tz A string of the tz for the date times.
 #' @param crs An integer of the EPSG for the projection of the points.
 #' @return An sf object with a tibble of the datetime and a sfc_POINT geometry of three dimensional points where the third dimension is the elevation in m.
+#' @seealso ps_read_tracks_gpxs
 #' @export
 ps_read_tracks_gpx <- function(file, tz = getOption("ps.tz", "UTC"), crs = getOption("ps.crs", 4326)) {
   check_string(file)
@@ -38,5 +39,39 @@ ps_read_tracks_gpx <- function(file, tz = getOption("ps.tz", "UTC"), crs = getOp
     sf::st_transform(crs = crs)
 
   gpx <- gpx[order(gpx$datetime),]
+  gpx
+}
+
+#' Read GPX
+#'
+#' Reads tracks from a gpx file.
+#'
+#' @param dir A string of the directory name.
+#' @param pattern A string of the pattern to use when searching for files.
+#' @param recursive A flag indicating whether to recurse into subdirectories.
+#' @inheritParams ps_read_tracks_gpx
+#' @return An sf object with a tibble of the datetime (POSIXct) and the file path (character) and a sfc_POINT geometry of three dimensional points where the third dimension is the elevation in m.
+#' @seealso ps_read_tracks_gpx
+#' @export
+ps_read_tracks_gpxs <- function(dir, pattern = "[.]gpx$", recursive = FALSE,
+                                tz = getOption("ps.tz", "UTC"),
+                                crs = getOption("ps.crs", 4326)) {
+  check_string(dir)
+  check_string(pattern)
+  check_flag(recursive)
+
+  if (!dir.exists(dir))
+    stop("directory '", dir, "' does not exist", call. = FALSE)
+
+  files <- list.files(dir, pattern = pattern, recursive = recursive, full.names = TRUE)
+  sfiles <- list.files(dir, pattern = pattern, recursive = recursive)
+
+  if (!length(files)) stop("there are no gpx files", call. = FALSE)
+
+  gpx <- lapply(files, ps_read_tracks_gpx, tz = tz, crs = crs) %>%
+    stats::setNames(sfiles) %>%
+    purrr::imap(function(x, name) {x$file <- name; x}) %>%
+    do.call(rbind, .)
+
   gpx
 }
---
+++ tests/testthat/test-read-tracks-gpxs.R
@@ -0,0 +1,15 @@
+context("read-tracks-gpxs")
+
+test_that("works", {
+
+  tracks <- ps_read_tracks_gpxs(system.file("gpx", package = "poisspatial"), tz = "PST8PDT", recursive = TRUE)
+
+  expect_is(tracks, "sf")
+  expect_identical(names(tracks), c("datetime", "file", "geometry"))
+  expect_identical(nrow(tracks), 29306L)
+  expect_identical(lubridate::tz(tracks$datetime), "PST8PDT")
+  expect_identical(lubridate::hour(tracks$datetime[1]), 21L)
+  expect_identical(sort(unique(tracks$file)), sort(c("20110401.gpx", "20110402.gpx", "sub2/20110403.gpx")))
+})
+
+
