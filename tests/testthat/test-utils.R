dd80d7cbfeb056f9b7bf0abdd4378f3b135bcf93
Joe Thorley <joe@poissonconsulting.ca>
2017-09-22 14:51:04 -0700

    extra test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,4 +16,5 @@ test_that("utils", {
   proj4string <- ps_get_proj4string(x)
   expect_identical(proj4string, "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs")
   expect_warning(ps_get_epsg(1))
+  expect_true(is_crs(proj4string))
 })
