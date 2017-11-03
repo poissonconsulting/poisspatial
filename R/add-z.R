f86f84286be05a4b696defc3fad4101b9bb80252
Joe Thorley <joe@poissonconsulting.ca>
2017-11-03 14:25:18 -0700

    fix note X and Y undeclared

--- R/add-z.R
+++ R/add-z.R
@@ -29,7 +29,7 @@ ps_sfc_add_z <- function(x, sfc_column = ps_active_sfc_name(x), z_column = "Elev
 
   if(remove_z){x <- x[, setdiff(names(x), z_column)]}
 
-  x %<>% dplyr::select(-X, -Y)
+  x %<>% dplyr::select_(~-X, ~-Y)
   x
 }
 
