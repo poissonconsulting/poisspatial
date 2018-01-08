62cdddd9bf08ccd7357fcb9588f35274b04f62c4
Joe Thorley <joe@poissonconsulting.ca>
2018-01-08 11:23:16 -0800

    add ddm2dd

--- R/dms.R
+++ R/dms.R
@@ -14,7 +14,7 @@ ps_dd2ddm <- function(x) {
   d <- floor(x)
   dm <- (x - d) * 60
   d[!is.na(negative) & negative] <- d[!is.na(negative) & negative] * -1
-  x <- paste0(d, "\u00B0 ", dm ,"'")
+  x <- paste(d, dm)
   x[is.na(negative)] <- NA
   x
 }
@@ -33,7 +33,7 @@ ps_dm2mds <- function(x) {
   negative <- x < 0
   m <- floor(x)
   ds <- (x - m) * 60
-  x <- paste0(m, "' ", ds , "\u0022")
+  x <- paste(m, ds)
   x[is.na(negative)] <- NA
   x
 }
@@ -55,7 +55,33 @@ ps_dd2dmds <- function(x) {
   dm <- (x - d) * 60
   mds <- ps_dm2mds(dm)
   d[!is.na(negative) & negative] <- d[!is.na(negative) & negative] * -1
-  x <- paste0(d, "\u00B0 ", mds)
+  x <- paste(d, mds)
   x[is.na(negative)] <- NA
+  x
+}
+
+#' Degrees and Decimal Minutes to Decimal Degrees
+#'
+#' @param x A character vector.
+#'
+#' @return A numeric vector.
+#' @export
+#'
+#' @examples
+#' ps_ddm2dd(c("70 39.24", NA, "-56 39.24"))
+ps_ddm2dd <- function(x) {
+  check_vector(x, c("", NA))
+  x %<>% strsplit(" ")
+  length <- vapply(x, length, 1L)
+  if(any(length > 2))
+    ps_error("ddm must be two real numbers separated by a single space")
+
+  negative <- vapply(x, function(x) grepl("^-", x)[1], TRUE)
+  is.na(negative) <- vapply(x, function(x) is.na(x[1]), TRUE)
+  x %<>% lapply(function(x) sub("^-", "", x)) %>%
+    lapply(as.numeric)
+  x[!is.na(negative)] %<>% lapply(function(x) x[1] + x[2] / 60)
+  x %<>% unlist()
+  x[!is.na(negative) & negative] %<>% magrittr::multiply_by(-1)
   x
 }
--- tests/testthat/test-dms.R
+++ tests/testthat/test-dms.R
@@ -2,12 +2,17 @@ context("dms")
 
 test_that("dms", {
   expect_identical(ps_dd2ddm(c(70.654, NA, -56.654)),
-                   c("70\u00B0 39.2399999999998'", NA, "-56\u00B0 39.2400000000002'"))
+                   c("70 39.2399999999998", NA, "-56 39.2400000000002"))
   expect_identical(ps_dd2ddm(c(NA_real_)),
                    c(NA_character_))
 
-  expect_identical(ps_dm2mds(c(10.654, NA)),c("10' 39.24\"", NA))
+  expect_identical(ps_dm2mds(c(10.654, NA)),c("10 39.24", NA))
 
   expect_identical(ps_dd2dmds(c(70.654, NA, -56.654)),
-                   c("70\u00B0 39' 14.3999999999869\"", NA, "-56\u00B0 39' 14.4000000000125\u0022"))
+                   c("70 39 14.3999999999869", NA, "-56 39 14.4000000000125"))
+
+  expect_identical(ps_ddm2dd(c("70 39.24", NA, "-56 39.24")),
+                   c(70.654, NA, -56.654))
+    expect_identical(ps_ddm2dd(ps_dd2ddm(c(70.654, NA, -56.654))),
+                     c(70.654, NA, -56.654))
 })
