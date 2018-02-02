37702bf4587bf0cfa116047677d4f3c8d69bf61b
Joe Thorley <joe@poissonconsulting.ca>
2018-02-01 16:00:42 -0800

    fix test centroid with tie breaker

--- tests/testthat/test-centroid.R
+++ tests/testthat/test-centroid.R
@@ -93,12 +93,13 @@ test_that("centroid1", {
   expect_equal(sort(ps_sfc_to_coords(cent.pt)$Y), c(50.00627, 50.00692),
                    tolerance = 0.000001)
 
+  pt <- rbind(pt, pt[pt$id == 2,])
+
   pt <- ps_sfcs_to_utm(pt)
-  print(pt)
   cent.pt <- ps_sfc_centroid1(pt, by = "color", nearest = TRUE)
   expect_true(inherits(cent.pt$geometry, "sfc_POINT"))
   expect_true(inherits(cent.pt, "sf"))
   expect_identical(sf::st_crs(cent.pt), sf::st_crs(pt))
   expect_identical(colnames(cent.pt), c("color", "geometry"))
-  expect_equal(sort(ps_sfc_to_coords(cent.pt)$X), c(495367, 495371), tolerance = 0.000001)
+  expect_equal(ps_sfc_to_coords(cent.pt)$X, c(495371, 495367), tolerance = 0.000001)
 })
