92991590431eb4ec92799dc2b750debaaff2cfb6
joethorley <joe@poissonconsulting.ca>
2019-08-08 10:28:50 -0700

    simplify test and reduce notifications

--- tests/testthat/test-coords.R
+++ tests/testthat/test-coords.R
@@ -5,13 +5,6 @@ test_that("ps_coords_to_sfc when missnig values", {
   x$Row <- 2:4
 
   y <- ps_coords_to_sfc(x, crs = 28992, activate = FALSE)
-  expect_identical(y, structure(list(Row = 2:4, geometry = structure(list(structure(c(1,
-1), class = c("XY", "POINT", "sfg")), structure(c(1, 10), class = c("XY",
-"POINT", "sfg")), structure(c(10, 1), class = c("XY", "POINT",
-"sfg"))), class = c("sfc_POINT", "sfc"), precision = 0, bbox = structure(c(xmin = 1,
-ymin = 1, xmax = 10, ymax = 10), class = "bbox"), crs = structure(list(
-    epsg = 28992L, proj4string = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.4171,50.3319,465.5524,-0.398957,0.343988,-1.87740,4.0725 +units=m +no_defs"), class = "crs"), n_empty = 0L)), row.names = c(NA,
--3L), class = "data.frame"))
 
   x$X[3] <- NA
   y2 <- ps_coords_to_sfc(x, crs = 28992, activate = FALSE)
