fb8c99036e6859b9cb82c607b7d085683340c6e0
Joe Thorley <joe@poissonconsulting.ca>
2021-03-26 12:55:11 -0700

    - Added `ps_elevation_google()`.

---
+++ R/elevation.R
@@ -0,0 +1,34 @@
+#' Add Z column to sfc based on google maps
+#'
+#' @param x The sf object.
+#' @param sfc_name A string of the sfc column name.
+#' @param key The string of the google maps elevation api key.
+#' @return The sf object with a Z column of the elevation in metres.
+#' @export
+ps_elevation_google <- function(x, sfc_name = ps_active_sfc_name(x),
+                                key = Sys.getenv("GOOGLE_MAPS_ELEVATION_API_KEY")) {
+  y <- ps_sfc_to_longlat(x, sfc_name = sfc_name) # does checking
+  chk::chk_string(key)
+
+  if(!nrow(x)) {
+    x$Z <- numeric(0)
+    return(x)
+  }
+  y$..RowID <- 1:nrow(y)
+  z <- y[!is.na(y$Latitude) & !is.na(y$Longitude),]
+  if(!nrow(z)) {
+    x$Z <- NA_real_
+    return(x)
+  }
+  z$lat <- z$Latitude
+  z$lon <- z$Longitude
+  z$Latitude <- NULL
+  z$Longitude <- NULL
+
+  z$Z <- googleway::google_elevation(z, key = key)$results$elevation
+  z <- z[c("..RowID", "Z")]
+  y <- dplyr::left_join(y, z, by = "..RowID")
+  y <- y[order(y$..RowID),]
+  x$Z <- y$Z
+  x
+}
---
+++ tests/testthat/test-elevation.R
@@ -0,0 +1,25 @@
+test_that("ps_elevation_google", {
+  x <- data.frame(X = c(1,1,10), Y = c(1,10,1))
+  x$Row <- 2:4
+
+  x <- ps_coords_to_sfc(x, crs = 28992, activate = FALSE)
+  x <- ps_activate_sfc(x)
+
+  e <- ps_elevation_google(x)
+  expect_identical(colnames(x), c("Row", "geometry"))
+  expect_identical(e[c("Row", "geometry")], x[c("Row", "geometry")])
+  expect_equal(e$Z, c(135.154434204102, 135.121871948242, 135.305969238281))
+})
+
+test_that("ps_elevation_google missing values", {
+  x <- data.frame(X = c(1,1,10), Y = c(1,NA,1))
+  x$Row <- 2:4
+
+  x <- ps_coords_to_sfc(x, crs = 28992, activate = FALSE)
+  x <- ps_activate_sfc(x)
+
+  e <- ps_elevation_google(x)
+  expect_identical(colnames(x), c("Row", "geometry"))
+  expect_identical(e[c("Row", "geometry")], x[c("Row", "geometry")])
+  expect_equal(e$Z, c(135.154434204102, 135.305969238281, NA))
+})
