4f3696eaf17c6a831582675cad56215420823b99
Evan <evanamiesgalonski@gmail.com>
2023-10-23 13:05:51 -0700

    edd epsg rds file to replace data retrieved from rgdal

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' epsg
+#'
+#' @format A data frame containing the European Petroleum Survey Group (EPSG) geodetic parameter dataset as distributed with PROJ.4 software (previously supplied by rgdal).
+#' @examples
+#' epsg
+"epsg"
+
