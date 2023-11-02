380ca98adca13221e15e8722b2b6798545d844cc
Evan <evanamiesgalonski@gmail.com>
2023-11-01 17:46:57 -0700

    additional info on EPSG codes and where to find

--- R/data.R
+++ R/data.R
@@ -1,6 +1,9 @@
 #' epsg
 #'
-#' @format A data frame containing the European Petroleum Survey Group (EPSG) geodetic parameter dataset as distributed with PROJ.4 software (previously supplied by rgdal).
+#' @format A data frame containing the European Petroleum Survey Group
+#' (EPSG) geodetic parameter dataset as distributed with PROJ.4 software
+#' (previously supplied by rgdal). Equivalent Proj4 strings as well as other
+#' information on coordinate systems can be found at https://spatialreference.org.
 #' @examples
 #' epsg
 "epsg"
