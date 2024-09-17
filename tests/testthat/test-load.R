139efd55787684490316cdbb9d54c529169419e1
Duncan Kennedy <duncan@poissonconsulting.ca>
2024-09-17 16:49:26 -0600

    added `get_null_device()` to get the virtual machine for R-CMD-checks so that tests don't fail

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,18 +1,18 @@
 test_that("can load multiple spatial files and files of a certain type", {
   dir <- system.file("files", package = "poisspatial")
 
-  sink("/dev/null")
+  sink(get_null_device())
   files <- ps_load_spatial(dir, recursive = T, pattern = ".sqlite")
   sink()
   expect_identical(length(files), 1L)
 
-  sink("/dev/null")
+  sink(get_null_device())
   files <- ps_load_spatial(dir, recursive = T, pattern = ".shp")
   sink()
   expect_identical(length(files), 2L)
   expect_true(is.na(sf::st_crs(polyna)))
 
-  sink("/dev/null")
+  sink(get_null_device())
   files <- ps_load_spatial(dir, recursive = T, pattern = NULL)
   sink()
   expect_identical(length(files), 4L)
@@ -20,7 +20,7 @@ test_that("can load multiple spatial files and files of a certain type", {
 
 test_that("can set crs when missing", {
   dir <- system.file("files", package = "poisspatial")
-  sink("/dev/null")
+  sink(get_null_device())
   files <- ps_load_spatial(dir, recursive = T, pattern = ".shp", crs = 26911)
   sink()
   expect_true(!is.na(sf::st_crs(polyna)))
@@ -31,7 +31,7 @@ test_that("rename and fun arguments work", {
   dir <- system.file("files", package = "poisspatial")
   rename <- function(x) {gsub("pol", "", x)}
   trans <- function(x) {x %<>% sf::st_transform(4326)}
-  sink("/dev/null")
+  sink(get_null_device())
   files <- ps_load_spatial(dir, recursive = T, pattern = ".shp", crs = 26911, rename = rename, fun = trans)
   sink()
   expect_identical(sf::st_crs(yna)$epsg, 4326L)
@@ -41,7 +41,7 @@ test_that("spatial database loads", {
   path <- system.file("files/ikeda.gpkg", package = "poisspatial")
   rename <- toupper
   trans <- function(x) {sf::st_transform(x, 3005)}
-  sink("/dev/null")
+  sink(get_null_device())
   ikeda <- ps_load_spatial_db(path = path, rename = rename, fun = trans)
   sink()
   expect_identical(sf::st_crs(SITE)$proj4string, "+proj=aea +lat_0=45 +lon_0=-126 +lat_1=50 +lat_2=58.5 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs")
